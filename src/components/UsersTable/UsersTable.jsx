8361a8ba4eb60a828af43e855a772615713cf5a0
Chackmoole <cebep-21@mail.ru>
2022-10-22 16:52:42 +0300

    Styled table

--- src/components/UsersTable/UsersTable.jsx
+++ src/components/UsersTable/UsersTable.jsx
@@ -1,39 +1,45 @@
 import React from 'react';
 import usersData from 'components/UsersTable/usersTableData';
 import {
+  StyledTableBox,
+  StyledTableInner,
   StyledTD,
   StyledTH,
-  StyledTR,
   StyledUsersTable,
+  StyledUserTableCaption,
 } from 'components/UsersTable/UsersTable.styled';
 
 const UserTable = () => {
   return (
-    <StyledUsersTable>
-      <caption>Данные пользователей</caption>
-      <thead>
-        <StyledTR>
-          <StyledTH>Имя</StyledTH>
-          <StyledTH>Фамилия</StyledTH>
-          <StyledTH>Статус</StyledTH>
-          <StyledTH>Почта</StyledTH>
-          <StyledTH>Дата регистрации</StyledTH>
-        </StyledTR>
-      </thead>
-      <tbody>
-        {usersData.map((user) => {
-          return (
-            <StyledTR key={user.id}>
-              <StyledTD>{user.firstName}</StyledTD>
-              <StyledTD>{user.lastName}</StyledTD>
-              <StyledTD>{user.status}</StyledTD>
-              <StyledTD>{user.mail}</StyledTD>
-              <StyledTD>{user.registrationDate}</StyledTD>
-            </StyledTR>
-          );
-        })}
-      </tbody>
-    </StyledUsersTable>
+    <StyledTableBox>
+      <StyledTableInner>
+        <StyledUsersTable>
+          <StyledUserTableCaption>Данные пользователей</StyledUserTableCaption>
+          <thead>
+            <tr>
+              <StyledTH>Имя</StyledTH>
+              <StyledTH>Фамилия</StyledTH>
+              <StyledTH>Статус</StyledTH>
+              <StyledTH>Почта</StyledTH>
+              <StyledTH>Дата регистрации</StyledTH>
+            </tr>
+          </thead>
+          <tbody>
+            {usersData.map((user) => {
+              return (
+                <tr key={user.id}>
+                  <StyledTD>{user.firstName}</StyledTD>
+                  <StyledTD>{user.lastName}</StyledTD>
+                  <StyledTD>{user.status}</StyledTD>
+                  <StyledTD>{user.mail}</StyledTD>
+                  <StyledTD>{user.registrationDate}</StyledTD>
+                </tr>
+              );
+            })}
+          </tbody>
+        </StyledUsersTable>
+      </StyledTableInner>
+    </StyledTableBox>
   );
 };
 
