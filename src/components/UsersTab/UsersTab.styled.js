5c03f9c588af14b2092c4530264de844e377a546
Chackmoole <cebep-21@mail.ru>
2022-10-23 10:49:41 +0300

    style UsersTabs

--- src/components/UsersTab/UsersTab.styled.js
+++ src/components/UsersTab/UsersTab.styled.js
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
 export const StyledUsersTab = styled.button`
-  min-height: 20px;
-  min-width: 80px;
-  padding: 8px;
+  min-height: 40px;
+  min-width: 60px;
+  padding: 12px 20px;
   border: none;
+  background: #ffffff;
+  border-radius: 4px;
 `;
