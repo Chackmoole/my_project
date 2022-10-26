a60c8281caa9dcdc8864f9462a776303640dc4fe
Chackmoole <cebep-21@mail.ru>
2022-10-26 14:49:38 +0300

    Добавлен кнопки outlined

--- src/ui/Button/Button.jsx
+++ src/ui/Button/Button.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { StyledButton } from 'src/ui/Button/Button.styled';
 
-const Button = (props) => {
-  return <StyledButton onClick={props.onClick}>{props.children}</StyledButton>;
+const Button = ({ children, onClick, variant = 'contained' }) => {
+  return (
+    <StyledButton onClick={onClick} variant={variant}>
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
--- src/ui/Button/Button.styled.js
+++ src/ui/Button/Button.styled.js
@@ -1,10 +1,17 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const StyledButton = styled.button`
   padding: 8px;
   font-size: 12px;
   background-color: royalblue;
   color: #e7ebf0;
-  border: 1px solid #e7ebf0;
   border-radius: 4px;
+  border: 1px solid royalblue;
+
+  ${(props) =>
+    props.variant === 'outlined' &&
+    css`
+      background-color: transparent;
+      color: royalblue;
+    `}
 `;
