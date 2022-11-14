e5a606f3085e42d402f7605689f2a4fe69238ed4
Chackmoole <cebep-21@mail.ru>
2022-11-14 11:49:06 +0300

    15) рефакторинг. Удален компонент CrossButton. Перенесен в компонент Modal

--- src/ui/Modal/Modal.jsx
+++ src/ui/Modal/Modal.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import CrossButton from 'src/ui/CrossButton/CrossButton';
 import Button from 'src/ui/Button/Button';
 import MuiDialog from '@mui/material/Dialog';
 import MuiDialogTitle from '@mui/material/DialogTitle';
 import MuiDialogContent from '@mui/material/DialogContent';
 import MuiDialogActions from '@mui/material/DialogActions';
+import { StyledCrossButton } from 'src/ui/Modal/Modal.styled';
 
 const Modal = ({ closeModal, title, children, onClickAction, open }) => {
   return (
     <MuiDialog open={open} onClose={closeModal}>
-      <CrossButton onClick={closeModal} />
+      <StyledCrossButton onClick={closeModal} />
       <MuiDialogTitle variant="h6" mb="24px">
         {title}
       </MuiDialogTitle>
