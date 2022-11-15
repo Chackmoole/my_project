6ecc3e5847e41dadf2081e29b66eeb5c605910dd
Chackmoole <cebep-21@mail.ru>
2022-11-15 19:04:28 +0300

    16) TS. Типизированы компоненты Footer & Header(без logo).

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 
 import Typography from '@mui/material/Typography';
 
-import logo from 'src/img/logo.png';
-
+// import logo from 'img/logo.png';
 import { StyledHeader } from 'components/Header/Header.styled';
 
 const Header = () => {
   return (
     <StyledHeader>
-      <img src={logo} alt="react logo" width={40} height={40} />
+      {/*<img src={logo} alt="react logo" width={40} height={40} />*/}
       <Typography variant="subtitle1">Страница пользователей</Typography>
     </StyledHeader>
   );
