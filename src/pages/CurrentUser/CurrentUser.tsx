f7a603de22323d3bab505d6707f472cd91762527
Chackmoole <cebep-21@mail.ru>
2022-12-20 17:50:26 +0300

    20) Изменения согласно комментариям. Добавлены TODO

--- src/pages/CurrentUser/CurrentUser.tsx
+++ src/pages/CurrentUser/CurrentUser.tsx
@@ -1,7 +1,6 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { ChangeEvent, useCallback, useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
-import MuiBox from '@mui/material/Box';
 import Box from '@mui/material/Box';
 import { SelectChangeEvent } from '@mui/material/Select';
 import { StyledCurrentUserBox } from 'pages/CurrentUser/CurrentUser.styled';
@@ -18,7 +17,7 @@ import Typography from 'ui/Typography/Typography';
 const CurrentUser = () => {
   const { userId } = useParams();
   const currentUser = useMemo(() => {
-    return USERS.find((user) => user.id == +userId);
+    return USERS.find((user) => user.id == Number(userId));
   }, [userId]);
   const [currentUserValues, setCurrentUserValues] = useState({
     firstName: currentUser.firstName,
@@ -37,45 +36,44 @@ const CurrentUser = () => {
   //navigation
   const navigate = useNavigate();
 
-  const backRedirect = useCallback(() => {
-    navigate(-1);
-  }, []);
-
   const onSaveButtonClick = useCallback(() => {
     console.log(currentUserValues);
   }, [currentUserValues]);
 
   const onBackButtonClick = useCallback(() => {
-    backRedirect();
-  }, []);
+    navigate(-1);
+  }, [navigate]);
+
+  //TODO Объединить в одну функцию
+  const onFirstNameChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setCurrentUserValues({ ...currentUserValues, firstName: e.target.value });
+    },
+    [currentUserValues]
+  );
+  const onLastNameChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setCurrentUserValues({ ...currentUserValues, lastName: e.target.value });
+    },
+    [currentUserValues]
+  );
+  const onMailChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setCurrentUserValues({ ...currentUserValues, mail: e.target.value });
+    },
+    [currentUserValues]
+  );
 
   return (
     <PageLayout>
       <StyledCurrentUserBox>
-        <EditableRow
-          onChange={(e) => {
-            setCurrentUserValues({ ...currentUserValues, firstName: e.target.value });
-          }}
-          value={currentUserValues.firstName}
-          label="Имя"
-        />
-        <EditableRow
-          onChange={(e) => {
-            setCurrentUserValues({ ...currentUserValues, lastName: e.target.value });
-          }}
-          value={currentUserValues.lastName}
-          label="Фамилия"
-        />
-        <EditableRow
-          onChange={(e) => {
-            setCurrentUserValues({ ...currentUserValues, mail: e.target.value });
-          }}
-          value={currentUserValues.mail}
-          label="Почта"
-        />
+        <EditableRow onChange={onFirstNameChange} value={currentUserValues.firstName} label="Имя" />
+        <EditableRow onChange={onLastNameChange} value={currentUserValues.lastName} label="Имя" />
+        <EditableRow onChange={onMailChange} value={currentUserValues.mail} label="Имя" />
 
-        <MuiBox sx={{ display: 'flex', gap: '40px', alignItems: 'center' }}>
+        <Box sx={{ display: 'flex', gap: '40px', alignItems: 'center' }}>
           <Typography sx={{ minWidth: '100px' }}>Статус</Typography>
+          {/*TODO баг с отображением статуса в Select*/}
           <Select
             label="Статус"
             options={STATUS_VARIANTS}
@@ -83,7 +81,7 @@ const CurrentUser = () => {
             value={currentUser.status}
             id="status"
           />
-        </MuiBox>
+        </Box>
         <Box sx={{ display: 'flex', gap: '40px' }}>
           <Button onClick={onSaveButtonClick}>Сохранить</Button>
           <Button onClick={onBackButtonClick} variant="outlined">
