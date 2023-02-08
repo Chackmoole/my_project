a70cfeacfea2e5ddf3f10c5b953784457922e6d7
Chackmoole <cebep-21@mail.ru>
2023-02-08 16:06:02 +0300

    mainPage) fix mobile height

--- src/components/PageLayout/PageLayout.styled.ts
+++ src/components/PageLayout/PageLayout.styled.ts
@@ -5,6 +5,7 @@ export const StyledPageLayout = styled.div`
   min-width: 540px;
   margin: 0 auto;
   min-height: 100vh;
+  height: calc(100vh - calc(100vh - 100%));
   display: flex;
   flex-direction: column;
   justify-content: space-between;
