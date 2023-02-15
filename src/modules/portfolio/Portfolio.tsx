5bc3999d46037a3eb7575f57477511580e42eae2
Chackmoole <cebep-21@mail.ru>
2023-02-15 19:53:57 +0300

    mainPage) Убран излишний компонент Main.

--- src/modules/portfolio/Portfolio.tsx
+++ src/modules/portfolio/Portfolio.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 
+import About from 'modules/portfolio/components/About/About';
+import Contacts from 'modules/portfolio/components/Contacts/Contacts';
 import Header from 'modules/portfolio/components/Header/Header';
-import Main from 'modules/portfolio/components/Main/Main';
+import PortfolioBlock from 'modules/portfolio/components/PortfolioBlock/PortfolioBlock';
+import Skills from 'modules/portfolio/components/Skills/Skills';
+import TitleBlock from 'modules/portfolio/components/TitleBlock/TitleBlock';
 
 const Portfolio = () => {
   return (
     <>
       <Header />
-      <Main />
+      <main>
+        <TitleBlock />
+        <PortfolioBlock />
+        <About />
+        <Skills />
+        <Contacts />
+      </main>
     </>
   );
 };
--- src/modules/portfolio/components/Main/Main.styled.ts
+++
@@ -1,5 +0,0 @@
-import styled from '@emotion/styled';
-
-export const StyledMain = styled.main`
-  min-width: 320px;
-`;
